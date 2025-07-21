286b2c0c8fe307a72bd70d90f4a79408f41333ef
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-07-21 15:32:42 +0200

    add first step of function addContact

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 10:36:37 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/07/21 12:35:24 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/07/21 15:10:36 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,70 @@
 #include <cctype>
 //TODO ADD
 //TODO SEARCH
-//TODO EXIT
+
+int	addContact(PhoneBook& phonebook)
+{
+	(void)phonebook;
+	if (std::cin.eof())
+		return (-2);
+	Contact	contact;
+	std::string	firstName, lastName, nickname, phoneNumber, darkestSecret;
+
+	std::cout << "Enter first name: ";
+    std::getline(std::cin, firstName);
+    if (std::cin.eof())
+    {
+        std::cout << "\nCTRL+D was pressed. Exiting addContact.\n";
+        return (-2);
+    }
+	std::cout << "Enter last name: ";
+    std::getline(std::cin, lastName);
+	if (std::cin.eof())
+    {
+        std::cout << "\nCTRL+D was pressed. Exiting addContact.\n";
+        return (-2);
+    }
+	std::cout << "Enter nickname: ";
+    std::getline(std::cin, nickname);
+	if (std::cin.eof())
+    {
+        std::cout << "\nCTRL+D was pressed. Exiting addContact.\n";
+        return (-2);
+    }
+	std::cout << "Enter phone number: ";
+    std::getline(std::cin, phoneNumber);
+	if (std::cin.eof())
+    {
+        std::cout << "\nCTRL+D was pressed. Exiting addContact.\n";
+        return (-2);
+    }
+	std::cout << "Enter darkest secret: ";
+    std::getline(std::cin, darkestSecret);
+	if (std::cin.eof())
+    {
+        std::cout << "\nCTRL+D was pressed. Exiting addContact.\n";
+        return (-2);
+    }
+  	std::cout << "" << firstName << " " << lastName << " " << nickname << " " << phoneNumber << " " << darkestSecret << "\n";
+	return (0);
+}
+
 
 int	main()
 {
-	std::string command;
+	PhoneBook	phonebook;
+	std::string	command;
 
 	while (true)
 	{
+		//TODO display information
         std::getline(std::cin, command);
 		if (command == "ADD")
-			printf("ADD\n");
-		if (command == "SEARCH")
+			addContact(phonebook);
+		else if (command == "SEARCH")
 			printf("SEARCH\n");
-		if (command == "EXIT")
+		else if (command == "EXIT")
 			break;
 	}
+	return (0);
 }
