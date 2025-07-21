411557b474e541a300b78c4bdffa34c007cb44d7
cyraullie <cyril.goldenschue@gmail.com>
2025-07-21 13:48:06 +0200

    add every command

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 10:36:37 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/07/21 11:49:18 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/07/21 12:35:24 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,9 @@ int	main()
         std::getline(std::cin, command);
 		if (command == "ADD")
 			printf("ADD\n");
+		if (command == "SEARCH")
+			printf("SEARCH\n");
+		if (command == "EXIT")
+			break;
 	}
 }
