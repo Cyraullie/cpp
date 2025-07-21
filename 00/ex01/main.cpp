d0471da7536fbdcebc5918471b345df37c640dc3
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-07-21 11:22:34 +0200

    add test to getline

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -6,9 +6,30 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 10:36:37 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/07/17 10:42:02 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/07/21 11:18:49 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 #include "Contact.hpp"
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <limits>
+#include <cctype>
+//TODO ADD
+//TODO SEARCH
+//TODO EXIT
+
+int	main()
+{
+	std::string command;
+
+	//while (true)
+	//{
+	std::cout << "commande : ";
+	std::getline(std::cin, command);
+	if (command == "ADD")
+		printf("ADD");
+	//}
+}
