df6ec18e38c4c0854d2a3c9ea6a70aecf296cd48
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-08-12 15:42:01 +0200

    start ex02

---
+++ 00/ex02/Account.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 15:30:22 by cgoldens          #+#    #+#             */
+/*   Updated: 2025/08/12 15:41:46 by cgoldens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <ostream>
+
+
+// Account::Account(int initial_deposit): _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0)
+// {
+
+// }
+// Account::~Account()
+// {
+
+// }
+
+// static int	getNbAccounts( void )
+// {
+
+// }
+
+// static int	getTotalAmount( void )
+// {
+
+// }
+
+// static int	getNbDeposits( void )
+// {
+
+// }
+
+// static int	getNbWithdrawals( void )
+// {
+
+// }
+
+// static void	displayAccountsInfos( void )
+// {
+
+// }
+
+
+// void	makeDeposit( int deposit )
+// {
+
+// }
+
+// bool	makeWithdrawal( int withdrawal )
+// {
+
+// }
+
+
+int		Account::checkAmount( void ) const
+{
+	return (this->_amount);
+}
+
+// void	displayStatus( void ) const
+// {
+
+// }
+
+
+// static void	_displayTimestamp( void )
+// {
+
+// }
