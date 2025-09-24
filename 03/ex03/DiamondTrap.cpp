a41aab0e40fcc4d26a6dadc8f724a7bb1decd6b7
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-09-24 15:57:34 +0200

    finish CPP03/ex03

--- 03/ex03/DiamondTrap.cpp
+++ 03/ex03/DiamondTrap.cpp
@@ -6,7 +6,67 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 13:05:20 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/09/24 13:05:21 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/09/24 15:38:28 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "DiamondTrap.hpp"
+
+
+DiamondTrap::DiamondTrap(): ClapTrap("Default DiamondTrap_clap_name"),FragTrap(), ScavTrap(), _name("Default DiamondTrap")
+{
+	this->_hp = 100; // from FragTrap
+	this->_damage = 30; // from FragTrap
+	std::cout << "Default DiamondTrap constructor called" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(std::string name): ClapTrap(name + "_clap_name"), FragTrap(), ScavTrap(), _name(name)
+{
+	this->_hp = 100; // from FragTrap
+	this->_damage = 30; // from FragTrap
+	std::cout << "DiamondTrap constructor called for " << this->_name << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &src): ClapTrap(src._name + "_clap_name") , FragTrap(), ScavTrap(src._name)
+{
+	this->_damage = src._damage;
+	this->_hp = src._hp;
+	this->_energy = src._energy;
+}
+
+DiamondTrap::~DiamondTrap()
+{
+	std::cout << "destructor called for" << this->_name << std::endl;
+}
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &cpy)
+{
+	if (this != &cpy)
+	{
+		this->_name = cpy._name;
+		ClapTrap::_name = cpy._name + "_clap_name";
+		this->_damage = cpy._damage;
+		this->_hp = cpy._hp;
+		this->_energy = cpy._energy;
+		std::cout << "Assignment operator DiamdonTrap called for "  << this->_name << std::endl;
+	}
+	return (*this);
+}
+
+void DiamondTrap::whoAmI()
+{
+	std::cout << "Name: " << this->_name << std::endl;
+	std::cout << "Clap_name: " << ClapTrap::_name << std::endl;
+}
+
+void DiamondTrap::displayAttributs()
+{
+	std::cout << "------------------------" << std::endl;
+	std::cout << "DiamondTrap" << std::endl;
+	std::cout << "Name: " << this->_name << std::endl;
+	std::cout << "Clap_name: " << ClapTrap::_name << std::endl;
+	std::cout << "Hit points: " << this->_hp << std::endl;
+	std::cout << "Energy points: " << this->_energy << std::endl;
+	std::cout << "Attack damage: " << this->_damage << std::endl;
+	std::cout << "------------------------" << std::endl;
+}
--- 03/ex03/main.cpp
+++ 03/ex03/main.cpp
@@ -6,32 +6,29 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 15:56:43 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/09/11 16:06:50 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/09/24 15:37:35 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FragTrap.hpp"
-#include "ScavTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int	main()
 {
-	ClapTrap a("Clappy");
-	a.attack("enemy1");
-	a.takeDamage(12);
+	std::cout << "Creating a DiamondTrap object..." << std::endl;
+	DiamondTrap diamond("Diamou");
+	DiamondTrap diamo("Diamo");
 
-	std::cout << "----------------------" << std::endl;
-
-	ScavTrap b("Scrappy");
-	b.attack("enemy2");
-	b.beRepaired(12);
-	b.guardGate();
-
-	std::cout << "----------------------" << std::endl;
-
-	FragTrap c("Fraga");
-	c.attack("Scrappy");
-	c.takeDamage(15);
-	c.highFivesGuys();
-
-	return 0;
+	diamond.displayAttributs();
+	diamo.displayAttributs();
+	diamo.attack("Molon");
+	diamo.displayAttributs();
+	std::cout << "Assigmement Diamo to diamond" << std::endl;
+	diamond = diamo;
+	diamond.takeDamage(10);
+	diamond.displayAttributs();
+	diamond.beRepaired(10);
+	diamond.displayAttributs();
+	std::cout << "new Diamou :" << std::endl;
+	diamond.displayAttributs();
+	diamo.whoAmI();
 }
