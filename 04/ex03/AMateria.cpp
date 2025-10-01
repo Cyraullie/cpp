a33b711842d6eb4c35b7cbfa921ecd7307599565
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-10-01 13:37:24 +0200

    finish AMateria setup

--- 04/ex03/AMateria.cpp
+++ 04/ex03/AMateria.cpp
@@ -6,23 +6,23 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 11:58:18 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/10/01 12:01:29 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/10/01 13:37:06 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
 
-AMateria::AMateria()
+AMateria::AMateria(): _type("void")
 {
 	
 }
 
-AMateria::AMateria(std::string const & type)
+AMateria::AMateria(std::string const & type): _type(type)
 {
 	
 }
 
-AMateria::AMateria(const AMateria& cpy)
+AMateria::AMateria(const AMateria& cpy): _type(cpy._type)
 {
 	
 }
@@ -34,13 +34,15 @@ AMateria::~AMateria()
 
 AMateria &AMateria::operator=(const AMateria &src)
 {
-	
+	if (this->_type != src._type)
+		this->_type = src._type;
+	return (*this);
 }
 
 
 std::string const & AMateria::getType() const
 {
-	
+	return (this->_type);
 }
 
 void AMateria::use(ICharacter& target)
