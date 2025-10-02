18dc76ece08f36be778a2252f510805fe4114d21
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-10-02 11:55:36 +0200

    add some text

--- 04/ex03/main.cpp
+++ 04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 11:40:02 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/10/01 15:39:35 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/10/02 11:55:03 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,109 +15,28 @@
 #include "Cure.hpp"
 #include "Character.hpp"
 
-void copy_constructor_test()
-{
-	IMateriaSource* src = new MateriaSource();
-	Ice *ice = new Ice();
-	Cure *cure = new Cure();
-	src->learnMateria(ice);
-	src->learnMateria(cure);
-	Character me("me");
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me.equip(tmp);
-	tmp = src->createMateria("cure");
-	me.equip(tmp);
-	Character copy(me);
-	me.unequip(0);
-	me.use(0, copy);
-	copy.use(0, me);
-	delete src;
-	delete ice;
-	delete cure;
-}
-
-void assignment_operator_test()
-{
-	IMateriaSource* src = new MateriaSource();
-	Ice *ice = new Ice();
-	Cure *cure = new Cure();
-	src->learnMateria(ice);
-	src->learnMateria(cure);
-	Character me("me");
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me.equip(tmp);
-	tmp = src->createMateria("cure");
-	me.equip(tmp);
-	Character copy;
-	copy = me;
-	me.unequip(0);
-	me.use(0, copy);
-	copy.use(0, me);
-	delete src;
-	delete ice;
-	delete cure;
-}
-
-void invalid_arguments_test()
-{
-	IMateriaSource* src = new MateriaSource();
-	Ice *ice = new Ice();
-	Cure *cure = new Cure();
-	src->learnMateria(ice);
-	src->learnMateria(0);
-	src->learnMateria(cure);
-	src->learnMateria(cure);
-	src->learnMateria(cure);
-	src->learnMateria(cure);
-	ICharacter* me = new Character("me");
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	tmp = src->createMateria("nonexistant");
-	me->equip(tmp);
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	ICharacter* bob = new Character("bob");
-	me->unequip(-1);
-	me->use(0, *bob);
-	me->use(1, *bob);
-	me->use(-1, *bob);
-	delete bob;
-	delete me;
-	delete src;
-	delete ice;
-	delete cure;
-}
-
 int main()
 {
-	 //subject_provided_test();
-	// copy_constructor_test();
-	// assignment_operator_test();
-	// invalid_arguments_test();
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
 	
 	ICharacter* me = new Character("me");
+	ICharacter* bob = new Character("bob");
 	
 	AMateria* tmp;
+	
+	std::cout << "0" << std::endl;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
+	me->use(0, *bob);
+	me->use(1, *bob);
+	me->use(2, *bob);
+	me->use(3, *bob);
+	
+	std::cout << "1" << std::endl;
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
-	
-	ICharacter* bob = new Character("bob");
-
-	me->use(0, *bob);
 	me->use(1, *bob);
 	
 	delete bob;
