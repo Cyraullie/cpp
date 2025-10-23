9152b18f68bbab0a1d81c537687b9b235f19c05b
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-10-23 13:35:53 +0200

    finish CPP07/ex01

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -6,7 +6,46 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 11:51:05 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/10/23 11:51:05 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/10/23 13:35:43 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ITER_HPP
+# define ITER_HPP
+		
+# include <cstdlib> 
+# include <iostream> 
+
+template <typename T>
+void iter(const T *array, size_t const len, void (*f)(const T&))
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		f(array[i]);
+	}
+}
+
+template <typename T>
+void iter(T *array, size_t const len, void (*f)(T&))
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		f(array[i]);
+	}
+}
+
+// ** FUNCTIONS TEST **
+
+template<typename T>
+void increment(T &elem)
+{
+	elem++;
+}
+
+template<typename T>
+void print(T const &elem)
+{
+	std::cout << elem << std::endl;
+}
+
+#endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -6,7 +6,52 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 11:51:07 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/10/23 11:51:08 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/10/23 13:34:59 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+#include "colors.hpp"
+
+int main(void)
+{
+	int tab[5] = {0, 1, 2, 3, 4};
+
+	std::cout << YELLOW << BOLD << "*** TRY WITH A TAB OF INT ***" << RESET << std::endl;
+	std::cout << "print: " << std::endl;
+	::iter(tab, 5, ::print);
+	std::cout << "increment & reprint: " << std::endl;
+	::iter(tab, 5, ::increment);
+	::iter(tab, 5, ::print);
+
+	char chars[11] = "testouille";
+
+	std::cout << YELLOW << BOLD << "*** TRY WITH A TAB OF CHAR ***" << RESET << std::endl;
+	std::cout << "print: " << std::endl;
+	::iter(chars, 11, ::print);
+	std::cout << "increment & reprint: " << std::endl;
+	::iter(chars, 11, ::increment);
+	::iter(chars, 11, ::print);
+
+	double doubles[6] = {2.5, 1.2, 4.2, 5.5, 7.0, 6.9};
+
+	std::cout << YELLOW << BOLD << "*** TRY WITH A TAB OF DOUBLE ***" << RESET << std::endl;
+	std::cout << "print: " << std::endl;
+	::iter(doubles, 6, ::print);
+	std::cout << "increment & reprint: " << std::endl;
+	::iter(doubles, 6, ::increment);
+	::iter(doubles, 6, ::print);
+
+	const int ctab[3] = {4, 2, 0};
+	
+	std::cout << YELLOW << BOLD << "*** TRY WITH A CONST TAB OF INT ***" << RESET << std::endl;
+	std::cout << "print: " << std::endl;
+	::iter(ctab, 3, ::print);
+	std::cout << "obviously we can't do increment" << std::endl;
+
+	std::string strings[3] = {"hello", "World", "42"};
+	
+	std::cout << YELLOW << BOLD << "*** TRY WITH A TAB OF STRINGS ***" << RESET << std::endl;
+	::iter(strings, 3, ::print);
+	std::cout << "we can't increment a string" << std::endl;
+}
