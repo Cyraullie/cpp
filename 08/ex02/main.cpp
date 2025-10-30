d5d0452fbbbdffdab8755779b7b5fce6815bb230
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-10-30 15:50:48 +0100

    add subject's main

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 14:18:42 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/10/30 15:47:26 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/10/30 15:50:32 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 int main(void)
 {
-	return (0);
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
